c0590e89dacae60a959ce04d0db347878b0fc242
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2012-10-14 17:19:05 +0900

    testに対してはscalaコンパイルできるようになった

--- src/main/java/com/example/sample/sas/action/HelloAction.scala
+++
@@ -1,6 +0,0 @@
-package com.example.sample.sas.action
-
-class HelloAction
-{
-    index = "index.jsp"
-}
---
+++ src/test/scala/HelloAction.scala
@@ -0,0 +1,6 @@
+package com.example.sample.sas.action
+
+class HelloAction
+{
+    def index = "index.jsp"
+}
